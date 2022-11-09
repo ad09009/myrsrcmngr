838e3ed76473a83f7b76a290467c894ae97ba991
ad09009 <ad09009@edu.lu.lv>
2022-11-09 02:00:28 +0200

    Added initial scans and hosts tables to models

--- myrsrcmngr/myrsrcmngr/models.py
+++ myrsrcmngr/myrsrcmngr/models.py
@@ -7,3 +7,26 @@ class Resource(models.Model):
     def __str__(self):
         return self.name
     
+class scans(models.Model):
+    call_date = models.DateTimeField(auto_now_add=True)
+    template_id = models.IntegerField()
+    status = models.SmallIntegerField()
+    parsed = models.BooleanField()
+    path_to_xml = models.CharField(max_length=500)
+    def __str__(self):
+        return 'Scan at ' + self.call_date
+    
+class hosts(models.Model):
+    main_address = models.CharField(max_length=50, blank=False)
+    ipv4 = models.CharField(max_length=50)
+    mac = models.CharField(max_length=50)
+    vendor = models.CharField(max_length=100)
+    ipv6 = models.CharField(max_length=50)
+    status = models.CharField(max_length=50)
+    hostnames = models.CharField(max_length=400)
+    os_fingerprint = models.CharField(max_length=200)
+    tcpsequence = models.CharField(max_length=50)
+    ipsequence = models.CharField(max_length=50)
+    uptime = models.CharField(max_length=50)
+    lastboot = models.CharField(max_length=50)
+    distance = models.IntegerField()
