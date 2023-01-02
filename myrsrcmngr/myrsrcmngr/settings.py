e46ebd3202d51350be44482d3ac5e596164c4b25
ad09009 <ad09009@edu.lu.lv>
2023-01-02 19:00:20 +0200

    Settings modifications

--- myrsrcmngr/myrsrcmngr/settings.py
+++ myrsrcmngr/myrsrcmngr/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k-3w2(=y)i*mb0xs)a8z&g%fe0akmcjgfn1i%+k)091bkp3p2_'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
